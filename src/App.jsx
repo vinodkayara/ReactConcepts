044aa3fe87fa8787c2f2e64443fcb094d03f030a
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2025-10-15 22:28:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 style={{height:"70px",width:"100%",border:"5px solid black" ,backgroundColor:"white"}}>
+        Welcome to React JS 
+      </h1>
+      <p style={{color:"blue",fontSize:"50px"}}>React js is javascript library used to create uswerInterface</p>
+    </div>
+  )
+}
+
+export default App

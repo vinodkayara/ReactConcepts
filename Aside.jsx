973ce57665626251cbc9e410c62af70928a20464
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2025-10-16 22:13:08 +0530

    Add files via upload

---
+++ Aside.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './aside.css'
+
+
+function Aside() {
+  return (
+    <div className='main1'>
+       <div className='as'></div>
+       <div className='main'></div>
+       </div>
+ 
+    
+  )
+}
+
+export default Aside
